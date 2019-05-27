45de705c6bff30adf331bafd5345a4f8c3aa1a83
SebastiaanYN <sebastiaanyn@gmail.com>
2019-05-27 22:22:11 +0200

    Use the @syntek/eslint-config-syntek package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,10 @@
 module.exports = {
+  extends: '@syntek/syntek',
   env: {
     browser: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'import',
-    'jsdoc',
-    'eslint-comments',
-  ],
   rules: {
-    'no-param-reassign': 'off', // fine for working with CodeMirror
-    'no-use-before-define': 'off', // fine for working with CodeMirror
-
-    'no-console': 'off',
-    'class-methods-use-this': 'off',
-    'require-await': 'error',
-    'no-restricted-syntax': 'off',
-    'prefer-destructuring': 'off',
-    'import/no-cycle': 'off',
-
-    // jsdoc
-    'jsdoc/check-alignment': 'error',
-    'jsdoc/check-examples': 'error',
-    'jsdoc/check-indentation': 'error',
-    'jsdoc/check-param-names': 'error',
-    'jsdoc/check-syntax': 'error',
-    'jsdoc/check-tag-names': 'error',
-    'jsdoc/check-types': 'error',
-    'jsdoc/newline-after-description': 'error',
-    'jsdoc/no-undefined-types': 'error',
-    'jsdoc/require-hyphen-before-param-description': 'error',
-    'jsdoc/require-param-description': 'error',
-    'jsdoc/require-param-name': 'error',
-    'jsdoc/require-param': 'error',
-    'jsdoc/require-returns-description': 'error',
-    'jsdoc/valid-types': 'error',
-
-    // eslint-comments
-    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
-    'eslint-comments/no-aggregating-enable': 'error',
-    'eslint-comments/no-duplicate-disable': 'error',
-    'eslint-comments/no-unlimited-disable': 'error',
-    'eslint-comments/no-unused-disable': 'error',
-    'eslint-comments/no-unused-enable': 'error',
+    'no-param-reassign': 'off',
+    'no-use-before-define': 'off',
   },
-  overrides: [
-    {
-      files: ['*.test.ts'],
-      rules: {
-        'no-unused-expressions': 'off',
-      },
-    },
-  ],
 };
